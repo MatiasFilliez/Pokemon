776699b03a597718d5ef8aad6a19594ddfbad1da
MatiasFilliez <matiifilliez@gmail.com>
2022-02-08 04:42:48 -0300

    feat(async_function)

--- api/src/controllers/cAuth.js
+++ api/src/controllers/cAuth.js
@@ -2,34 +2,31 @@ const { User } = require("../db")
 const bcrypt = require("bcrypt")
 const jwt = require("jsonwebtoken")
 
-const singIn = (req, res) => {
-    const { email, password } = req.body;
-    const user = User.findOne({
-        where: {
-            email: email
-        }
-    }).then(findUser => {
-        if (!findUser) {
-            res.status(404).json({ msg: "user not found" })
-        }
+const singIn = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const findUser = await User.findOne({ where: { email: email } })
+        if (!findUser) { res.status(404).json({ msg: "user not found" }) }
         else if (!bcrypt.compareSync(password, findUser.password)) {
             res.status(404).json({ msg: "password incorrect" })
         } else {
             let token = jwt.sign({ user: findUser }, "token", { expiresIn: "7d" })
             res.json({ user: findUser, token: token, })
         }
-    })
+    } catch (err) { console.log(err) }
 }
 
-const singUp = (req, res) => {
-    const { email, name, password } = req.body;
-    let encryptPassword = bcrypt.hashSync(password, 10)
-    User.create({ name: name, email: email, password: encryptPassword })
-        .then(user => {
-            let token = jwt.sign({ user: user }, "token", { expiresIn: "7d" })
-            res.json({ user: user, token: token, })
-        })
-        .catch(err => res.status(404).json(err))
+const singUp = async (req, res) => {
+    try {
+        const { email, name, password } = req.body;
+        let encryptPassword = bcrypt.hashSync(password, 10)
+        const userCreated = await User.create({ name: name, email: email, password: encryptPassword })
+        let token = jwt.sign({ user: userCreated }, "token", { expiresIn: "7d" })
+        res.json({ user: userCreated, token: token, })
+    } catch (err) {
+        console.log(err)
+    }
+
 }
 
 module.exports = {
